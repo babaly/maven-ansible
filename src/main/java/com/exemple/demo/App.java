2a58f0e04e3f9e6e7dd1a96de5fa48b3739e562e
maven <maven@gmail.com>
2023-02-10 16:17:40 +0100

    initial commit

---
+++ src/main/java/com/exemple/demo/App.java
@@ -0,0 +1,25 @@
+package com.exemple.demo;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int x, y, sum;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez le premier nombre: ");
+		x = sc.nextInt();
+		System.out.println("Entrez le second nombre: ");
+		y = sc.nextInt();
+
+		sum = x + y;
+
+		System.out.println("La somme des deux nombres est: " + sum);
+    }
+
+}	
